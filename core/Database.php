bbf9d2578ff65fa53cc115cde26720361e1e69f5
Christopher Edynak <christopher1edynak@gmail.com>
2021-01-11 11:04:52 -0500

    implemented PDO connection script

--- core/Database.php
+++ core/Database.php
@@ -1,3 +1,21 @@
 <?php 
+    $user = 'root';
+    $dsn = 'mysql:host=localhost; dbname=sbrs';
+    $dbPassword = '';
 
+    try{
+        //create an instance of the PDO class with the required parameters
+        $db = new PDO($dsn, $user, $dbPassword);
+
+        //set pdo error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+        //display success message
+        //echo "You have successfully connected to the db";
+
+    }catch (PDOException $ex){
+        //display error message
+        echo "Connection failed ".$ex->getMessage();
+    }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+    include_once 'core/Database.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
