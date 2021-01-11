11149803b861365d8bd55f9534c4be9b5cda5b07
Christopher Edynak <christopher1edynak@gmail.com>
2021-01-11 10:42:48 -0500

    implemented file structure and intialized app

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Security Based Registration System</title>
+</head>
+<body>
+    
+</body>
+</html>
